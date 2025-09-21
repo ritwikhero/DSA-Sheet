651cd636bbf7fe7e364dadd633bde6e91df3a2e0
Ritwik <ritwikhero@gmail.com>
2025-09-22 00:39:44 +0530

    optimal solutiion

--- src/Arrays/Hard/LongestSubarrayWithSum0.java
+++ src/Arrays/Hard/LongestSubarrayWithSum0.java
@@ -1,5 +1,7 @@
 package Arrays.Hard;
 
+import java.util.HashMap;
+
 public class LongestSubarrayWithSum0 {
     static int solve(int[] a){
         int  max = 0;
@@ -19,5 +21,33 @@ public class LongestSubarrayWithSum0 {
     {
         int a[] = {9, -3, 3, -1, 6, -5};
         System.out.println(solve(a));
+    }
+    int maxLen(int A[], int n)
+    {
+        // Your code here
+        HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
+
+        int maxi = 0;
+        int sum = 0;
+
+        for(int i = 0;i<n;i++) {
+
+            sum += A[i];
+
+            if(sum == 0) {
+                maxi = i + 1;
+            }
+            else {
+                if(mpp.get(sum) != null) {
+
+                    maxi = Math.max(maxi, i - mpp.get(sum));
+                }
+                else {
+
+                    mpp.put(sum, i);
+                }
+            }
+        }
+        return maxi;
     }
 }
