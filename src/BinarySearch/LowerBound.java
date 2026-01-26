4da6339173afc9cb5f71a9a7c428355f8fb93faa
Ritwik <ritwikhero@gmail.com>
2026-01-26 12:31:24 +0530

    implement lower bound

---
+++ src/BinarySearch/LowerBound.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class LowerBound {
+    public int lowerBound(int[] arr, int x) {
+        int low = 0;                  // Start index
+        int high = arr.length - 1;    // End index
+        int ans = arr.length;         // Default value if not found
+
+        while (low <= high) {
+            int mid = (low + high) / 2;  // Find mid index
+
+            if (arr[mid] >= x) {
+                ans = mid;            // Store possible answer
+                high = mid - 1;       // Move left
+            } else {
+                low = mid + 1;        // Move right
+            }
+        }
+        return ans;  // Return the lower bound index
+    }
+}
