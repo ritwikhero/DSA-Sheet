bbfc8ae6db6950d2e56d7bdf3c085e057d4a7b72
Ritwik <ritwikhero@gmail.com>
2026-01-30 21:29:00 +0530

    search insert position added

---
+++ src/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class SearchInsertPosition {
+    int n = arr.length;
+    int low = 0, high = n - 1;
+    int ans = n; // Default to end if x is greater than all elements
+
+        while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] >= x) {
+            // Potential answer found, try to go left
+            ans = mid;
+            high = mid - 1;
+        } else {
+            // Go right
+            low = mid + 1;
+        }
+    }
+
+        return ans;
+}
